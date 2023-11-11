8e9a5ea7d058daa7dcaf4202c4fa8ad7d7928c25
cueltschey <chaseuelt@gmail.com>
2023-11-11 17:29:22 -0600

    finished the pdfViewer implementation

--- app/src/main/java/com/example/android_booksreader/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/android_booksreader/ui/home/HomeFragment.kt
@@ -1,15 +1,26 @@
 package com.example.android_booksreader.ui.home
 
+import android.media.SoundPool
+import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import com.example.android_booksreader.R
 import com.example.android_booksreader.databinding.FragmentHomeBinding
 import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.io.File
 
 class HomeFragment : Fragment() {
 
@@ -37,7 +48,47 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        pdfView = root.findViewById(R.id.pdfView)
+
+        val storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
+
+        downloadPdf("pdfs/lab 7 screenshots.pdf")
+
         return root
+    }
+    private fun downloadPdf(pdfFilePath: String) {
+        val localFile = File.createTempFile("tempPdf", "pdf")
+        val localFileUri = localFile.toUri()
+
+        val pdfRef = storageRef.child(pdfFilePath)
+
+        pdfRef.getFile(localFile)
+            .addOnSuccessListener {
+                // PDF downloaded successfully
+                displayPdf(localFileUri)
+            }
+            .addOnFailureListener { exception ->
+                // Handle unsuccessful download
+                Toast.makeText(context, "Unable to load: $exception", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun displayPdf(pdfUri: Uri) {
+        pdfView.visibility = View.VISIBLE
+        pdfView.fromUri(pdfUri)
+            .onLoad(object : OnLoadCompleteListener {
+                override fun loadComplete(nbPages: Int) {
+                    // Do something when PDF is loaded
+                }
+            })
+            .onPageChange(object : OnPageChangeListener {
+                override fun onPageChanged(page: Int, pageCount: Int) {
+                    // Do something when page changes
+                }
+            })
+            .load()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/android_booksreader/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android_booksreader/ui/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.android_booksreader.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.barteksc.pdfviewer.PDFView
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +11,10 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _pdfView = MutableLiveData<PDFView>().apply {
+        value = null
+    }
+
+    val pdfView: LiveData<PDFView> = _pdfView
 }
